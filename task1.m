18205f311e0ed4dfac9abd0f1467e4cd7f8a0622
marhjoh <marhjoh@stud.ntnu.no>
2023-04-03 09:01:08 +0200

    add: image segmentation method - thresholding

--- task1.m
+++ task1.m
@@ -5,3 +5,8 @@ img_tiger = imread("images/tiger.jpg");
 % Convert the images to grayscale
 img_flower_gray = rgb2gray(img_flower);
 img_tiger_gray = rgb2gray(img_tiger);
+
+% Perform thresholding
+threshold = 128; % Set the threshold value
+binary_img_flower = img_flower_gray > threshold;
+binary_img_tiger = img_tiger_gray > threshold;
