877027399cd5f909538e79cf8396f9686cd9d9ff
marhjoh <marhjoh@stud.ntnu.no>
2023-04-03 09:29:29 +0200

    add: display threshold result

--- thresholding.m
+++ thresholding.m
@@ -10,3 +10,10 @@ img_tiger_gray = rgb2gray(img_tiger);
 threshold = 128; % Set the threshold value
 binary_img_flower = img_flower_gray > threshold;
 binary_img_tiger = img_tiger_gray > threshold;
+
+% Display the original image and the segmented images
+figure;
+subplot(2,2,1); imshow(img_flower_gray); title('Original Image');
+subplot(2,2,2); imshow(img_tiger_gray); title('Original Image');
+subplot(2,2,3); imshow(binary_img_flower); title('Thresholding');
+subplot(2,2,4); imshow(binary_img_tiger); title('Thresholding');
