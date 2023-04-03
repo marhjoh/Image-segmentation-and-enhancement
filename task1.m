f79129e817406f8f881c6738f67a8f10da3d8a0f
marhjoh <marhjoh@stud.ntnu.no>
2023-04-03 08:57:52 +0200

    add: read and convert images to grayscale

--- task1.m
+++ task1.m
@@ -1,5 +1,7 @@
+% Read in the images
+img_flower = imread("images/flower.jpg");
+img_tiger = imread("images/tiger.jpg");
 
-
-
-
-
+% Convert the images to grayscale
+img_flower_gray = rgb2gray(img_flower);
+img_tiger_gray = rgb2gray(img_tiger);
